a9046577dee6b490570351468620e9fee8a06098
Kelttipekoni <mikko.saurento@helsinki.fi>
2021-04-06 11:07:47 +0300

    Lisätty hahmotelma dictionarysta ja käyttäjä profiilin tallennuksesta

--- src/budget.py
+++ src/budget.py
@@ -1,6 +1,31 @@
 
 class Budget:
-
+    user = ""
+    psw = ""
+    profiles = {"Username": user, "Password": psw}
     def __init__(self, user:str, psw: str):
         self.user = user
         self.psw = psw
+        
+
+    def new_user():
+
+        while True:
+            print("Syötä käyttäjänimi ja salasana, salasanan pituus tulee olla vähintään 8 merkkiä!")
+            nimi = input("Käyttäjä:")
+            sala = input("Salasana:")
+            while len(sala)<8:
+                print("Liian lyhyt salasana, yritä uudestaan")
+                sala = input("Salasana:")
+
+            profiles = {nimi : sala}
+            print(profiles)
+           # print(nimi, sala)
+            break
+
+    
+userr = Budget.new_user()
+
+#if __name__ == "__main__":
+
+    #Budget.new_user()
