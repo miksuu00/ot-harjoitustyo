abb818d6b894765bfe2ddefb097d0c95b28bca75
Kelttipekoni <mikko.saurento@helsinki.fi>
2021-04-27 21:07:34 +0300

    parit fiksit kutsuihin

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -25,7 +25,7 @@ class UI:
     def _show_main_screen(self):
         self._hide_current_view()
         self._current_view = LoginMain(
-        self._root, self._show_login_view, self._show_new_user_view)
+        self._root, self._show_login_view, self._show_new_user_view, self._show_budget_view, self._show_list_view)
 
     def _show_new_user_view(self):
         self._hide_current_view()
