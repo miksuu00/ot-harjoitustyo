5e4b91f735203dac2d84c927bc41f162931ed1a4
Kelttipekoni <mikko.saurento@helsinki.fi>
2021-04-27 20:24:18 +0300

    Extra graphifks

--- src/ui/list_view.py
+++ src/ui/list_view.py
@@ -2,11 +2,12 @@ from tkinter import ttk, constants, StringVar
 from services.budget_service import BudgetService
 
 class ListView:
-    def __init__(self, handle_show_list_view, handle_show_login):
+    def __init__(self, handle_show_list_view, handle_show_login, budget_list):
         self._show_login_view = handle_show_login
         self._handle_show_list_view = handle_show_list_view
-
-        self._pa
+        self._list = ttk.Listbox(budget_list)
+        self._show_button =ttk.Button(budget_list, text='Budget List', command=show_expenses)
+        
 
     def show_expenses(self):
         select = self._list.curselection()
